a8cfb1549ceb23447f3a0300d54d35d92e3a55bf
sergeipeturov <peturov.s@gmail.com>
2021-05-23 13:00:45 +0500

    concentration realization

---
+++ MeditationGameV2/Assets/Scripts/Objects/ShieldScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldScript : MonoBehaviour
+{
+    private void Awake()
+    {
+        GameManager.Instance.PlayerManager.ThoughtAddedNotify += PlayerManager_ThoughtAddedNotify;
+        GameManager.Instance.PlayerManager.ThoughtRemovedNotify += PlayerManager_ThoughtRemovedNotify;
+
+        if (!GameManager.Instance.PlayerManager.PosistiveThoughts.Any(x => x.Name == Names.Positive_2_Concentration))
+            this.gameObject.SetActive(false);
+    }
+
+    private void PlayerManager_ThoughtAddedNotify(Thought thought)
+    {
+        if (thought.Name == Names.Positive_2_Concentration)
+            this.gameObject.SetActive(true);
+    }
+
+    private void PlayerManager_ThoughtRemovedNotify(Thought thought)
+    {
+        if (thought.Name == Names.Positive_2_Concentration)
+            this.gameObject.SetActive(false);
+    }
+}
